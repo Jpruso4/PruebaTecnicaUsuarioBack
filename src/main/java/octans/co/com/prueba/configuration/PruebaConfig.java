aa5ff252ef22fbe0190e3ff59720c70ac32455e6
Jpruso4 <juanpablo01282002@gmail.com>
2022-02-22 04:09:46 -0500

    Cambiando la url del cors de front permitida

--- src/main/java/octans/co/com/prueba/configuration/PruebaConfig.java
+++ src/main/java/octans/co/com/prueba/configuration/PruebaConfig.java
@@ -14,7 +14,7 @@ public class PruebaConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("https://pruebausuario-d56eb.web.app")
                         .allowedMethods("*")
                         .maxAge(3600);
             }
